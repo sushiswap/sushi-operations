addd4d881b2c5a2ffd5186d664a0b8ffd14e17e9
0xJiro <jiro@sushi.com>
2023-03-01 16:37:10 -0500

    pulling LP balances in maker job

---
+++ jobs/sushi-maker/src/main.py
@@ -0,0 +1,88 @@
+import sys
+import os
+import json
+import time
+import math
+import argparse
+from web3 import Web3, HTTPProvider
+from web3.logs import DISCARD
+from eth_abi import encode
+
+from utils.graph_data import fetch_lp_tokens
+
+# Retrieve job-defined env vars
+TASK_INDEX = os.getenv("CLOUD_RUN_TASK_INDEX", 0)
+TASK_ATTEMPT = os.getenv("CLOUD_RUN_TASK_ATTEMPT", 0)
+RUN_PRIORITY = False
+RUN_NON_PRIORITY = False
+IGNORE_GAS = False
+
+# Retrieve user-defined env vars
+MAINNET_RPC_URL = os.environ["MAINNET_RPC_URL"]
+ARBITRUM_RPC_URL = os.environ["ARBITRUM_RPC_URL"]
+OPS_ADDRESS = os.environ["OPS_ADDRESS"]
+OPS_PK = os.environ["OPS_PK"]
+
+
+RPC_URL = {
+    "arbitrum": ARBITRUM_RPC_URL,
+}
+
+WETH_SERVER_ADDRESSES = {
+    "arbitrum": "0xa19b3b22f29E23e4c04678C94CFC3e8f202137d8",
+}
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "abis/WethMaker.json")) as f:
+    WETH_MAKER_ABI = json.load(f)
+
+
+def main(chain):
+    print("Running WethMaker Job")
+    print(f"Starting Task {TASK_INDEX} Attempt {TASK_ATTEMPT}...")
+
+    w3 = Web3(Web3.HTTPProvider(RPC_URL[chain]))
+    if not w3.isConnected():
+        print(f"Failed to connect to {chain}")
+        return RuntimeError
+
+    last_block = w3.eth.get_block("latest")
+    # next_gas_price
+    # gas price checks if should run script goes here
+    print(f"Current block: {last_block['number']}")
+
+    print("Fetching LP token balances...")
+    lp_tokens_data = fetch_lp_tokens(
+        WETH_SERVER_ADDRESSES[chain], chain)
+    print(f"Fetched {len(lp_tokens_data)} LP tokens to burn...")
+
+    burn_lp_tokens(w3, lp_tokens_data)
+
+    # fetch all lp tokens maker is holding
+
+    # process and structure data to make burn calls
+
+    # burn all lp tokens into individual tokens
+
+    # sell all tokens for weth, watch for no bridge set and large slippage that should revert tx
+
+    unwind_lp_tokens(w3)
+
+
+def burn_lp_tokens(w3, lp_tokens_data):
+    for lp_token in lp_tokens_data:
+        if lp_token['pair']['trackedReserveETH'] == '0':
+            print(f"Bunk pair: {lp_token['pair']['name']}")
+            continue
+        usd_value = float(lp_token['pair']['reserveUSD']) * (
+            float(lp_token['liquidityTokenBalance']) / float(lp_token['pair']['totalSupply']))
+        print(f"Burning ${usd_value} worth of {lp_token['pair']['name']}")
+        print(lp_token['pair']['id'])
+
+
+def unwind_lp_tokens(w3):
+
+    return
+
+
+if __name__ == "__main__":
+    main("arbitrum")
---
+++ jobs/sushi-maker/src/utils/graph_data.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+EXCHANGE_GRAPH_ENDPOINTS = {
+    "arbitrum": "https://api.thegraph.com/subgraphs/name/sushiswap/exchange-arbitrum-backup"
+}
+
+
+def fetch_lp_tokens(address, chain):
+    user_lp_query = """query($addy: String!) {
+    user(id: "0xa19b3b22f29e23e4c04678c94cfc3e8f202137d8") {
+      liquidityPositions(first: 1000, orderBy: liquidityTokenBalance, orderDirection: desc) {
+        pair {
+          id
+          token0Price
+          token1Price
+          name
+          reserve0
+          reserve1
+          totalSupply
+          reserveUSD
+          trackedReserveETH
+        }
+        liquidityTokenBalance
+        }
+      }
+    }
+    """
+
+    variables = {"addy": address}
+    result = requests.post(
+        EXCHANGE_GRAPH_ENDPOINTS[chain], json={
+            "query": user_lp_query, "variables": variables}
+    )
+    data = json.loads(result.text)
+    user_lp_tokens = data['data']['user']['liquidityPositions']
+
+    sorted_lp_tokens = sorted(
+        user_lp_tokens,
+        key=lambda d: float(d['pair']['reserveUSD']) * (
+            float(d['liquidityTokenBalance']) / float(d['pair']['totalSupply'])),
+        reverse=True
+    )
+
+    return sorted_lp_tokens
