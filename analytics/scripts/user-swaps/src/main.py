b12bce0078885f4e393af8101f2d55f8509a0325
0xJiro <jiro@sushi.com>
2023-08-29 13:52:14 +0800

    add ethereum pulls to user-swaps script

--- analytics/scripts/user-swaps/src/main.py
+++ analytics/scripts/user-swaps/src/main.py
@@ -143,12 +143,12 @@ if __name__ == "__main__":
     pull_swaps(
         "ethereum", # network
         [
-            "", # weth
-            "", # sushi
-            "", # wbtc
-            "", # usdc
-            "", # usdt
-            "", #dai
+            "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", # weth
+            "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2", # sushi
+            "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", # wbtc
+            "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", # usdc
+            "0xdac17f958d2ee523a2206206994597c13d831ec7", # usdt
+            "0x6b175474e89094c44da98b954eedeac495271d0f", #dai
         ], # tokens to whitelist
         10, # usd value
         1693008000, # start timestamp
